70d72fb649987be302c0eba0333eba50f450f2cf
Vu Duy Tu <duytu131995@gmail.com>
2020-05-09 10:04:59 +0700

    add new pin

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,52 @@
+class PinsController < ApplicationController
+	before_action :find_pin, only: [:show, :edit, :destroy, :update]
+
+	def index
+		@pins = Pin.all.order("created_at ASC")
+	end
+
+	def new
+		@pin = Pin.new
+	end
+
+	def create
+		@pin = Pin.new(params_pin)
+
+		if @pin.save
+			redirect_to @pin, notice: "Successfully created new pin"
+		else
+			render 'new'
+		end
+	end
+
+	def show
+		
+	end
+
+	def edit
+		
+	end
+
+	def update
+		if @pin.update(params_pin)
+			redirect_to @pin, notice: 'Pin was Successfully'
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@pin.destroy
+		redirect_to root_path
+	end
+
+	private
+
+	def find_pin
+		@pin = Pin.find(params[:id])
+	end
+
+	def params_pin
+		params.require(:pin).permit(:title, :description)
+	end
+end
